d2893fb3c8991179925f584ba838b71588106265
jfsaine <jfsaine@gmail.com>
2023-09-01 21:40:06 -0400

    Update

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -1,1 +1,14 @@
-const x = input;
+function askUser() {
+    var userInput = prompt("Please enter some input:", "Default value");
+    
+    if (userInput != null) {
+        console.log("User entered: " + userInput);
+        return userInput;
+    } else {
+        console.log("User cancelled or entered nothing.");
+        return null;
+    }
+}
+
+// To use the function
+var response = askUser();
